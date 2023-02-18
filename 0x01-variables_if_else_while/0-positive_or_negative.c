a4c03a13ef81ac5033310d7788a89c490428bded
abiodunadeniji <abiodunadeniji19@gmail.com>
2023-02-17 18:51:25 -0800

    for variables f else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,22 +5,24 @@
 
 /* betty style doc for function main goes there */
 /**
- * main - This program will assign a random number to the variable n
- * Return: 0
- */
+* main - This program will assign a random number to the variable n
+* Return: 0
+*/
 int main(void)
 {
-  int n;
+int n;
 
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  if (n > 0)
-    {
-      printf("%d is positive\n", n);
-    } else if (n == 0)
-    {
-      printf("%d is zero\n", n);
-    } else
-    printf("%d is negative\n", n);
-  return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+printf("%d is negative\n", n);
+return (0);
 }
