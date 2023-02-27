34c95881182d107d259d801c2c47525183e35071
abiodunadeniji <abiodunadeniji19@gmail.com>
2023-02-26 18:48:12 -0800

    this is a commit message for task 5 minor changes

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - this function prints a string to the Std output
+ *
+ * Retrn: (0)
+ *
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+}
