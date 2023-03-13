9980462086f56f2fa065e6985c2a83c1e6e40c0c
abiodunadeniji <abiodunadeniji19@gmail.com>
2023-03-13 08:34:34 -0700

    You mustn't be afraid to dream a little bigger, darling

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,6 +4,8 @@
 /**
  * factorial - produce the factorial of a given number
  *
+ * @n: number used to return the factorial
+ *
  * Return: 0;
  */
 
@@ -11,14 +13,14 @@ int factorial(int n)
 {
 if (n < 0)
 {
-return -1;
+return (-1);
 }
 else if (n == 0)
 {
-return 1;
+return (1);
 }
 else
 {
-return n * factorial(n - 1);
+return (n * factorial(n - 1));
 }
 }
