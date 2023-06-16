7a91deb9e30dbaea63aa0cbb9c73e7d7c9cbd696
root <Abiodun Adeniji>
2023-06-16 03:07:24 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,17 @@
+#include "main.h"
+
 /**
  * _puts_recursion - Prints a string followed by a new line.
  * @s: The string to be printed.
  */
-
-#include "main.h"
-
-void _puts_recursion(char *s) {
-  if (*s == '\0')
+void _puts_recursion(char *s)
 {
-  _putchar('\n');
-  return;
-}
+  if (*s == '\0')
+    {
+      _putchar('\n');
+      return;
+    }
+
   _putchar(*s);
   _puts_recursion(s + 1);
 }
