483dbb50296f67a1dff4e397245cf58737d49c39
Abiodun adeniji <abiodunadeniji19@gmail.com>
2023-09-06 08:28:49 +0100

    Delete 0x18-dynamic_libraries directory

--- 0x18-dynamic_libraries/README.md
+++
@@ -1,1 +0,0 @@
-This directory is for shared libraries projects  in C
--- 0x18-dynamic_libraries/_isalpha.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-int _isalpha(int c) {
-  if (c >= 'A' && c <= 'Z') {
-    return 1;
-  } else {
-    return 0;
-  }
-}
